7cf73119e956b83610440c9d3fb13ae76def1690
sans08004-2 <00001123911654sp@al.educacao.sp.gov.br>
2026-04-29 15:59:07 -0300

    Create create.php
    esqueleto do site onde os Funcionarios colocam o cardapio no database

---
+++ create.php
@@ -0,0 +1,21 @@
+<p include 'connect.php'>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>a</title>
+</head>
+<body>
+    <input type="text" id="segundafeira" placeholder="Comida segunda">
+    <input type="text" id="tercafeira" placeholder="Comida treça">
+    <input type="text" id="quartafeira" placeholder="Comida quarta">
+    <input type="text" id="quintafeira" placeholder="Comida quinta">
+    <input type="text" id="sextafeira" placeholder="Comida sexta">
+
+
+
+
+
+</body>
+</html>
